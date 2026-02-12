9385a0a1a1e268c3810717d91dd9034f52be0f27
Sourav Kumar <codey@sourav.local>
2026-02-12 17:22:39 +0530

    feat: add supabase client

---
+++ src/lib/supabase.ts
@@ -0,0 +1,17 @@
+import { createClient } from "@supabase/supabase-js";
+
+const supabaseUrl = process.env.SUPABASE_URL;
+const supabaseKey = process.env.SUPABASE_KEY;
+
+if (!supabaseUrl || !supabaseKey) {
+  throw new Error(
+    "SUPABASE_URL and SUPABASE_KEY must be set in environment variables",
+  );
+}
+
+const supabase = createClient(
+  process.env.SUPABASE_URL!,
+  process.env.SUPABASE_KEY,
+);
+
+export default supabase;
