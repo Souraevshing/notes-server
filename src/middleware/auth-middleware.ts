e660fb2bdd14706669a163ca65809ce3cc564005
Sourav Kumar <codey@sourav.local>
2026-02-12 18:00:08 +0530

    feat: add auth middleware

---
+++ src/middleware/auth-middleware.ts
@@ -0,0 +1,27 @@
+import { type User } from "@supabase/supabase-js";
+import { NextFunction, Request, Response } from "express";
+
+import supabase from "../lib/supabase";
+
+declare global {
+  namespace Express {
+    interface Request {
+      user?: User;
+    }
+  }
+}
+
+async function authMiddleware(req: Request, res: Response, next: NextFunction) {
+  const token = req.headers.authorization?.split(" ")[1];
+  if (!token) {
+    return res.status(401).json({ error: "No token provided" });
+  }
+
+  const { data, error } = await supabase.auth.getUser(token);
+
+  if (error || !data.user)
+    return res.status(401).json({ error: "Invalid token" });
+  req.user = data.user;
+
+  next();
+}
